eabf3d43e2daba5a296cca19cc685883bd405bd5
Rohit <rohitamar@gmail.com>
2025-02-13 20:46:30 -0500

    first - added the library, just testing commits too

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # imagefilter
 
-Basic Image Filter Library in C++. Goal is to use SIMD to speed up computation. 
+Basic Image Filter Library in C++. Goal is to use SIMD to speed up computation. Here's a link to the image processing libraries that I used: https://github.com/nothings/stb/tree/master
---
+++ main.cpp
@@ -0,0 +1,28 @@
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
+#include <iostream>
+
+int main() {
+    int width, height, channels;
+    unsigned char* img = stbi_load("image.png", &width, &height, &channels, 0);
+    
+    if (!img) {
+        std::cerr << "Failed to load image\n";
+        return 1;
+    }
+
+    std::cout << "Image loaded: " << width << "x" << height << ", Channels: " << channels << "\n";
+
+    for (int y = 0; y < height; y++) {
+        for (int x = 0; x < width; x++) {
+            int index = (y * width + x) * channels;
+            unsigned char r = img[index];
+            unsigned char g = img[index + 1];
+            unsigned char b = img[index + 2];
+            std::cout << "Pixel(" << x << "," << y << "): R=" << (int)r << " G=" << (int)g << " B=" << (int)b << "\n";
+        }
+    }
+
+    stbi_image_free(img); // Free memory
+    return 0;
+}
